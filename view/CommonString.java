078ffef2e9e5b164d55d37171aa5cab745fd7293
Fuuka Adachi <futarokun@gmail.com>
2015-04-28 10:58:17 +0700

    add view and img

---
+++ view/CommonString.java
@@ -0,0 +1,8 @@
+package view;
+
+public class CommonString {
+
+	public static final String TITLE = "Virtual Project Assistant";
+	public static final String LogInBG = "img/banner.jpg";
+	public static final String LogInIC = "img/icon.jpg";
+}
---
+++ view/LogIn.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class LogIn extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public LogIn() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
